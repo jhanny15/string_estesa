b1878e6af205e20125bdff14283060a4842bf68a
jhanny15 <jhannycortes@gmail.com>
2021-05-03 11:32:04 +0200

    Aggiunto il test suite e StringEstesaTest

---
+++ src/test/string/StringEstesaTest.java
@@ -0,0 +1,53 @@
+package string;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringEstesaTest {
+
+    StringEstesa string = new StringEstesa();
+
+    @Test //1.a
+    void Test_isEmpty_null() {
+        Assertions.assertEquals(string.isEmpty(), StringUtils.isEmpty(string.getValue()));
+    }
+
+    @Test //1.b
+    void Test_isEmpty_Vuota() {
+        string.setValue("");
+        Assertions.assertEquals(string.isEmpty(), StringUtils.isEmpty(string.getValue()));
+    }
+
+    @Test //2.a
+    void Test_isBlank_null() {
+        Assertions.assertEquals(string.isBlank(), StringUtils.isBlank(string.getValue()));
+    }
+
+
+    @Test //2.b
+    void Test_isBlank_Vuota() {
+        string.setValue("");
+        Assertions.assertEquals(string.isBlank(), StringUtils.isBlank(string.getValue()));
+    }
+
+    @Test //2.c
+    void Test_isBlank_Spazio() {
+        string.setValue(" ");
+        Assertions.assertEquals(string.isBlank(), StringUtils.isBlank(string.getValue()));
+    }
+
+    @Test //3.a
+    void isNumeric() {
+        string.setValue("030521");
+        Assertions.assertEquals(string.isNumeric(), StringUtils.isNumeric(string.getValue()));
+    }
+
+    @Test //3.b
+    void isNumeric_Char() {
+        string.setValue(" 030521");
+        Assertions.assertEquals(string.isNumeric(), StringUtils.isNumeric(string.getValue()));
+    }
+}
